59b547f7177530d1eede479e18db945efd06181b
Jon Balon <development@jonbalon.com>
2022-01-12 23:47:45 -0500

    SQL File to Create `developers` Table

---
+++ server/.db/create-tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE developers (
+  id                  SERIAL
+                      PRIMARY KEY
+                      NOT NULL,
+  id_hash             TEXT
+                      NOT NULL
+                      DEFAULT concat('developer_', REPLACE(gen_random_uuid()::text, '-', '')),
+  name                VARCHAR(255)
+                      NOT NULL,
+  address_line        VARCHAR(255),
+  address_line2       VARCHAR(255),
+  address_city        VARCHAR(255),
+  address_state       VARCHAR(255),
+  address_postalcode  VARCHAR(255),
+  address_country     VARCHAR(255),
+  website             VARCHAR(255),
+  UNIQUE(id_hash)
+);
