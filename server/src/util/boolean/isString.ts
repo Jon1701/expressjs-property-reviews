c3ef0654a30c12c7c4018ce1d7c57a3e21efc7bb
Jon Balon <development@jonbalon.com>
2022-01-16 00:40:53 -0500

    Boolean Helper Function to Check if Argument is a String

---
+++ server/src/util/boolean/isString.ts
@@ -0,0 +1,9 @@
+/**
+ * Checks if the given argument is a string.
+ *
+ * @param item Argument to check if it is a string.
+ * @returns Whether or not the given argument is a string.
+ */
+const isString = (item: any): boolean => typeof item === "string";
+
+export default isString;
