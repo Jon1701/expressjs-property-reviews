a5f7b2ec2985c16a08fa92071594dc44fb947821
Jon Balon <development@jonbalon.com>
2022-01-20 13:23:17 -0500

    Remove Database Connection Functions Which Used `pg` Dependency

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,4 +1,3 @@
-import { Pool, Result as TransactionResult } from "pg";
 import { Sequelize, DataTypes } from "sequelize";
 
 const RADIX = 10;
@@ -15,51 +14,4 @@ const db = new Sequelize(
   }
 );
 
-// Connection information.
-const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT,
-  host: process.env.POSTGRES_HOST,
-});
-
-/**
- * Executes a transaction.
- *
- * @param query SQL query.
- * @param argsArray Array of arguments.
- * @returns {Promise<TransactionResult>} Transaction result.
- */
-const executeTransaction = async (
-  query: string,
-  argsArray
-): Promise<TransactionResult> => {
-  const client = await pool.connect();
-
-  try {
-    await client.query("BEGIN");
-    const result = await client.query(query, argsArray);
-    await client.query("COMMIT");
-
-    const payload: TransactionResult = {
-      result,
-      error: null,
-    };
-
-    return payload;
-  } catch (e) {
-    const result = await client.query("ROLLBACK");
-
-    const payload: TransactionResult = {
-      result: null,
-      error: result,
-    };
-
-    return payload;
-  } finally {
-    client.release();
-  }
-};
-
-export { db, executeTransaction, TransactionResult };
+export { db };
