5c320c0355e350043e4fc7c019859cd2a7a698e2
Jon Balon <development@jonbalon.com>
2022-01-19 21:21:20 -0500

    `Developer` Model

---
+++ server/src/models/developers/index.ts
@@ -0,0 +1,67 @@
+import { Sequelize, DataTypes } from "sequelize";
+import { db } from "@db/db";
+
+// Postgres connection string.
+const connString = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
+
+// Maximum allowed length for string fields.
+const MAX_STR_LEN = 255;
+
+// Developer model.
+const Developer = db.define(
+  "Developer",
+  {
+    developerID: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+      field: "id_hash",
+    },
+    name: {
+      type: DataTypes.STRING(MAX_STR_LEN),
+      allowNull: false,
+      field: "name",
+    },
+    addressLine1: {
+      type: DataTypes.STRING(MAX_STR_LEN),
+      allowNull: false,
+      field: "address_line1",
+    },
+    addressLine2: {
+      type: DataTypes.STRING(MAX_STR_LEN),
+      allowNull: true,
+      field: "address_line2",
+    },
+    addressCity: {
+      type: DataTypes.STRING(MAX_STR_LEN),
+      allowNull: false,
+      field: "address_city",
+    },
+    addressState: {
+      type: DataTypes.STRING(MAX_STR_LEN),
+      allowNull: false,
+      field: "address_state",
+    },
+    addressPostalCode: {
+      type: DataTypes.STRING(MAX_STR_LEN),
+      allowNull: false,
+      field: "address_postalcode",
+    },
+    addressCountry: {
+      type: DataTypes.STRING(MAX_STR_LEN),
+      allowNull: false,
+      field: "address_country",
+    },
+    website: {
+      type: DataTypes.STRING(MAX_STR_LEN),
+      allowNull: true,
+      field: "website",
+    },
+  },
+  {
+    tableName: "developers",
+    underscored: true,
+  }
+);
+
+export { Developer };
