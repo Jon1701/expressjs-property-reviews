8d9f166b43c79c4e9d540a11af31e4baa2e65de3
Jon Balon <development@jonbalon.com>
2022-01-20 20:12:51 -0500

    Configure Jest To Use Paths From `tsconfig.json`

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,7 +1,14 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+
+const { compilerOptions } = require("./tsconfig");
+
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/src/"],
   modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 };
