055e88d061855503156bc93e39caf869dce0ef11
Jon Balon <development@jonbalon.com>
2022-01-31 21:20:08 -0500

    Add `CREATE TABLE` Statement to Create `management_companies` Table

--- server/.db/create-tables.sql
+++ server/.db/create-tables.sql
@@ -30,3 +30,31 @@ CREATE TABLE developers (
                       NOT NULL,
   UNIQUE(id_hash)
 );
+
+CREATE TABLE management_companies (
+  id                  SERIAL
+                      PRIMARY KEY
+                      NOT NULL,
+  id_hash             UUID
+                      NOT NULL
+                      DEFAULT uuid_generate_v4(),
+  name                VARCHAR(255)
+                      NOT NULL,
+  address_line1       VARCHAR(255)
+                      NOT NULL,
+  address_line2       VARCHAR(255),
+  address_city        VARCHAR(255)
+                      NOT NULL,
+  address_state       VARCHAR(255)
+                      NOT NULL,
+  address_postalcode  VARCHAR(255)
+                      NOT NULL,
+  address_country     VARCHAR(255)
+                      NOT NULL,
+  website             VARCHAR(255),
+  created_at          TIMESTAMPTZ
+                      NOT NULL,
+  updated_at          TIMESTAMPTZ
+                      NOT NULL,
+  UNIQUE(id_hash)
+);
