d6baf34f42c8bcd237b83b9cf54c466b9b0987e2
Jon Balon <development@jonbalon.com>
2022-02-02 13:38:45 -0500

    `ManagementCompanyModel` Interface for the `ManagementCompany` Model

---
+++ server/src/interfaces/management.ts
@@ -0,0 +1,14 @@
+// Defines the fields used by the Management Company model.
+interface ManagementCompanyModel {
+  managementID?: string;
+  name?: string;
+  addressLine1?: string;
+  addressLine2?: string;
+  addressCity?: string;
+  addressState?: string;
+  addressPostalCode?: string;
+  addressCountry?: string;
+  website?: string;
+}
+
+export { ManagementCompanyModel };
