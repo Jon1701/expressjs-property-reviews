9a22941daa1622a3e7cd4b1bb21a8874a2d2bb24
Jon Balon <development@jonbalon.com>
2022-02-02 17:48:37 -0500

    Check if `developerID` URL Parameter Is Valid and If Database Row Exists in `PATCH /api/developers/:developerID`

--- server/src/controllers/api/developers/index.ts
+++ server/src/controllers/api/developers/index.ts
@@ -1,13 +1,53 @@
-import { Request, Response } from "express";
-import Sequelize from "sequelize";
+import e, { Request, Response } from "express";
+import Sequelize, { Model } from "sequelize";
 
-import { Developer as InterfaceDeveloper } from "@interfaces/developers";
+import {
+  Developer,
+  Developer as InterfaceDeveloper,
+} from "@interfaces/developers";
 import { Developer as ModelDeveloper } from "@models/developers";
 import { mapObjectToModel, mapModelToObject } from "@json/developers";
 import isObjectEmpty from "@util/boolean/isObjectEmpty";
 import isValidUUID from "@util/boolean/isValidUUID";
 
 import { validatePostObject, validatePatchObject } from "./validate";
+
+/**
+ * Returns row from the database.
+ *
+ * @param developerID Developer ID.
+ * @returns Row from the database as an object.
+ */
+const findOneDeveloper = async (
+  developerID: string
+): Promise<InterfaceDeveloper> => {
+  try {
+    const row = await ModelDeveloper.findOne({
+      attributes: [
+        "developerID",
+        "name",
+        "addressLine1",
+        "addressLine2",
+        "addressCity",
+        "addressState",
+        "addressPostalCode",
+        "addressCountry",
+        "website",
+      ],
+      where: {
+        developerID,
+      },
+    });
+
+    if (row === null) {
+      throw new Error();
+    }
+
+    return row.get();
+  } catch (e) {
+    return null;
+  }
+};
 
 /**
  * Route handler for GET /api/developers.
@@ -126,6 +166,12 @@ const patchDevelopers = async (req: Request, res: Response): Promise<void> => {
     return;
   }
 
+  const resultFindOne = await findOneDeveloper(req.params.developerID);
+  if (resultFindOne === null) {
+    res.status(404).send("Developer ID does not exist");
+    return;
+  }
+
   const results: InterfaceDeveloper = validatePatchObject(req.body);
   if (!isObjectEmpty(results)) {
     res.status(400).json(results);
@@ -133,14 +179,23 @@ const patchDevelopers = async (req: Request, res: Response): Promise<void> => {
   }
 
   try {
-    const result = await ModelDeveloper.update(mapObjectToModel(req.body), {
-      returning: true,
-      where: {
-        developerID: req.params.developerID,
-      },
-    });
+    const [numAffectedRows, arrayAffectedRows] = await ModelDeveloper.update(
+      mapObjectToModel(req.body),
+      {
+        returning: true,
+        where: {
+          developerID: req.params.developerID,
+        },
+      }
+    );
 
-    res.status(200).send(mapModelToObject(result[1][0].get()));
+    let responseBody: Developer = null;
+    if (numAffectedRows === 0) {
+      responseBody = {};
+    } else {
+      responseBody = mapModelToObject(arrayAffectedRows[0].get());
+    }
+    res.status(200).send(responseBody);
   } catch (err) {
     res.status(500).send();
   }
